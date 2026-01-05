2b7ec51d9a4fbfec3d9e46df3fa14ab590a42c78
Juan Manuel Rios <juanmanuelrios5602@gmail.com>
2026-01-05 12:56:31 -0300

    Add files via upload

---
+++ src/main/java/JMR/SistemaRRHH/Repository/EmpleadoRepositorio.java
@@ -0,0 +1,10 @@
+package JMR.SistemaRRHH.Repository;
+
+import JMR.SistemaRRHH.Model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
+
+
+
+}
